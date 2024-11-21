97f480afc4d1c9b51e653726fcdc158fa7db8731
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-11-21 19:36:03 +0600

    add first problrm

---
+++ ass_1.js
@@ -0,0 +1,19 @@
+/*function signature/sample */
+
+function calculateTax(income, expenses) {
+    if (
+        (typeof income !== 'number' && typeof expenses !== 'number') &&  
+        (income < 0 || expenses < 0)){
+        return "Invalid Input";
+    }
+    else if (expenses > income)  {
+        return "Invalid Input";
+    }    
+    else {
+        netProfit = (income - expenses) * .20;
+        return netProfit;
+    }
+}
+
+const result = calculateTax(10000, 3000);
+console.log(result);
